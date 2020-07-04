020b18d708e9bed6dc9fdbc1f5529e1b46360d7c
domitalk <dominic.m.chu@gmail.com>
2020-07-04 18:42:20 -0400

    basic textInput and Buttonn and also about the inline styling

--- App.js
+++ App.js
@@ -1,16 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    // style={} requires an object. thus the double curley
+    <View style={{padding: 50}}>
+      {/* view is most like div but unlike HTML you can't just put text inside the view, it has to be inside text component */}
+      {/* it's always nice to start with view because its the most flexible in terms of styling and because you need one wrapping compoenent at the top level */}
+      <View>
+        <TextInput placeholder="Course Goal" style={{borderColor: 'black', borderWidth: 1, padding: 10 }}/> 
+        {/* in docs you can see what kind of styling object key-value pairs you can use on the native elements */}
+
+        <Button title="add" />
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
+
+// later on probably better to actually put all the inline styling into this section but for now just going to style attr it 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
